524506734c8746a57b4f24114e3f04efef8153b3
Julius Frost <juliustfrost@gmail.com>
2020-06-24 14:37:02 -0400

    ppo hyperparams

--- train_ppo.py
+++ train_ppo.py
@@ -5,9 +5,10 @@ from ray import tune
 
 parser = argparse.ArgumentParser()
 parser.add_argument('env', type=str, help='environment name')
-parser.add_argument('--gpus', type=int, default=0)
-parser.add_argument('--workers', type=int, default=1)
-parser.add_argument('--output-dir', default='./data')
+parser.add_argument('--gpus', type=int, default=1)
+parser.add_argument('--workers', type=int, default=10)
+parser.add_argument('--output-dir', default='./data/ppo')
+parser.add_argument('--output-max-file-size', default=5000000)
 
 
 def main():
@@ -21,7 +22,22 @@ def main():
             'num_gpus': args.gpus,
             'num_workers': args.workers,
             'output': os.path.join(args.output_dir, args.env),
-            'output_max_file_size': 5000000,
+            'output_max_file_size': args.output_max_file_size,
+            'lambda': 0.95,
+            'kl_coeff': 0.5,
+            'clip_rewards': True,
+            'clip_param': 0.1,
+            'vf_clip_param': 10.0,
+            'entropy_coeff': 0.01,
+            'train_batch_size': 5000,
+            'rollout_fragment_length': 100,
+            'sgd_minibatch_size': 500,
+            'num_sgd_iter': 10,
+            'num_envs_per_worker': 5,
+            'batch_mode': 'truncate_episodes',
+            'observation_filter': 'NoFilter',
+            'vf_share_layers': True,
+            'use_pytorch': True
         },
     )
 
