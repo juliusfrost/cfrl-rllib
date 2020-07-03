0489b5b2c728bc3a0bee5a5618435c7ca8d18d7b
Olivia Watkins <oliviagracewatkins@gmail.com>
2020-07-03 08:46:32 -0700

    add critical states, untested

--- state_selection.py
+++ state_selection.py
@@ -16,7 +16,24 @@ def user_state(data, num_states, user, **kwargs):
 
 
 def critical_state(data, num_states, policy, **kwargs):
-    state_indices = None
+    state_indices = []
+    for trajectory in data.all_trajectories:
+        for obs_index in trajectory.observation_range:
+            obs = data.all_observations[obs_index]
+            logits, _ = policy.model.from_batch({"obs": obs})
+            action_dist = policy.dist_class(logits, policy.model)
+            entropy = action_dist.entropy()
+
+            if len(state_indices) < num_states:
+                state_indices.append(entropy, obs_index)
+            else:
+                last = state_indices[-1]
+                if entropy < last[0]:
+                    state_indices[len(state_indices) - 1] = (entropy, obs_index)
+                    state_indices.sort()
+
+    # Strip out the entropies
+    state_indices = [state for entropy, state in state_indices]
     return state_indices
 
 
