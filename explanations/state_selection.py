78b9d0abac4d84f9856c91e11bccd8b044c7816e
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2020-08-07 14:35:13 -0400

    simplify

--- explanations/state_selection.py
+++ explanations/state_selection.py
@@ -5,33 +5,31 @@ TODO: Implement
 """
 import torch
 
-def random_state(data, num_states, **kwargs):
+from explanations.data import Data
+
+
+def random_state(data: Data, num_states, **kwargs):
     state_indices = None
     return state_indices
 
 
-def user_state(data, num_states, user, **kwargs):
+def user_state(data: Data, num_states, user, **kwargs):
     state_indices = user.query(data)
     return state_indices
 
 
-def critical_state(data, num_states, policy, **kwargs):
+def critical_state(data: Data, num_states, policy, **kwargs):
     state_indices = []
     for obs_index, obs in enumerate(data.all_observations):
-        logits, _ = policy.model.from_batch({"obs": torch.FloatTensor(obs).unsqueeze(0)})
-        action_dist = policy.dist_class(logits, policy.model)
-        entropy = action_dist.entropy()
+        with torch.no_grad():
+            logits, _ = policy.model.from_batch({"obs": torch.FloatTensor(obs).unsqueeze(0)})
+            action_dist = policy.dist_class(logits, policy.model)
+            entropy = action_dist.entropy().item()
+        state_indices.append((entropy, obs_index))
 
-        if len(state_indices) < num_states:
-            state_indices.append((entropy, obs_index))
-        else:
-            last = state_indices[-1]
-            if entropy < last[0]:
-                state_indices[len(state_indices) - 1] = (entropy, obs_index)
-                state_indices.sort()
-
+    state_indices.sort(key=lambda x: x[0])
     # Strip out the entropies
-    state_indices = [state for entropy, state in state_indices]
+    state_indices = [state for entropy, state in state_indices[:num_states]]
     return state_indices
 
 
