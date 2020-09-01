09488c318831bd4d6f01f36686afeb1e043d06bd
Eric Weiner <eric@mcweiner.org>
2020-09-01 09:53:16 -0700

    Pipe through lane switching params

--- envs/driving.py
+++ envs/driving.py
@@ -14,14 +14,18 @@ def env_creator(**kwargs):
         game_name=kwargs.get('game_name', 'Driving'),
         display_screen=False,
         num_steps=NUM_STEPS,
+        switch_prob=kwargs.get('switch_prob', 0.0),
+        switch_duration = kwargs.get('switch_duration', 50),
         action_dim=kwargs.get('action_dim', 2)
     )
     return env
 
 
 def register(**kwargs):
+    switch_prob = kwargs.get('switch_prob', 0.0)
+    switch_duration = kwargs.get('switch_duration', 50)
     env_name = 'DrivingAccPLE-v0'
-    env_func = lambda _: env_creator(game_name="Driving", action_dim=2)
+    env_func = lambda _: env_creator(game_name="Driving", action_dim=2, switch_prob=switch_prob, swtich_duration=switch_duration)
     register_env(env_name, env_func)
 
     env_name = 'DrivingPLE-v0'
