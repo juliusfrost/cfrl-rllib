a578f4f329df2432a0dfd26ae51ecfd1576b6fea
Julius <juliustfrost@gmail.com>
2020-09-11 17:21:44 -0400

    fix default parameters

--- envs/driving.py
+++ envs/driving.py
@@ -27,7 +27,7 @@ def env_creator(**kwargs):
         game_name=kwargs.get('game_name', 'Driving'),
         display_screen=False,
         num_steps=NUM_STEPS,
-        switch_prob=kwargs.get('switch_prob', 0.0),
+        switch_prob=kwargs.get('switch_prob', 0.),
         switch_duration=kwargs.get('switch_duration', 50),
         action_dim=kwargs.get('action_dim', 2)
     )
@@ -39,7 +39,7 @@ def register():
     def driving_acc_env_factory(env_config):
         return env_creator(game_name="Driving",
                            action_dim=env_config.get('action_dim', 2),
-                           switch_prob=env_config.get('switch_prob', 50),
+                           switch_prob=env_config.get('switch_prob', 0.),
                            switch_duration=env_config.get('switch_duration', 50))
 
     register_env('DrivingPLE-v0', driving_acc_env_factory)
