7c943c402f38efd501f8beba2b83086a3dc6ebaf
Eric Weiner <eric@mcweiner.org>
2020-09-20 10:41:26 -0700

    Before adding video text

--- explanations/generate_counterfactuals.py
+++ explanations/generate_counterfactuals.py
@@ -20,41 +20,10 @@ register()
 def get_env_creator(env_name):
     return _global_registry.get(ENV_CREATOR, env_name)
 
-
-def add_border(imgs, border_size=10, border_color=[255,255,255]):
-    final_images = []
-    for img in imgs:
-        img_with_border = cv2.copyMakeBorder(
-            img,
-            top=border_size,
-            bottom=border_size,
-            left=border_size,
-            right=border_size,
-            borderType=cv2.BORDER_CONSTANT,
-            value=border_color
-        )
-        final_images.append(img_with_border)
-    return final_images
-
-def add_text(images, trajectory_rewards):
-    final_images = []
-    font = cv2.FONT_HERSHEY_SIMPLEX 
-    org = (50, 50) 
-    fontScale = 1
-    color = (255, 0, 0) 
-    thickness = 2
-    for i, image in enumerate(images):
-        new_image = cv2.putText(image, f'Timestep: {i}', org, font,  
-                        fontScale, color, thickness, cv2.LINE_AA)
-        final_images.append(new_image)
-    return final_images
-
-
-
 def write_video(frames, filename, image_shape, fps=5):
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     writer = cv2.VideoWriter(filename, fourcc, fps, image_shape)
-    frames = add_text(frames, None)
+    # frames = add_text(frames, None)
     for img in frames:
         writer.write(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
 
@@ -137,9 +106,9 @@ def select_states(args):
             exp_trajectory = exploration_dataset.get_trajectory(exp_id)
             cf_trajectory = counterfactual_dataset.get_trajectory(cf_id)
             original_imgs = original_trajectory.image_observation_range
-            original_imgs = add_border(original_imgs, border_color=[255,255,255])
-            exp_imgs = add_border(exp_trajectory.image_observation_range, border_color=[255,0,255])
-            cf_imgs = add_border(cf_trajectory.image_observation_range, border_color=[0,255,0])
+            original_imgs = original_imgs
+            exp_imgs = exp_trajectory.image_observation_range
+            cf_imgs = cf_trajectory.image_observation_range
             img_shape = (cf_imgs[0].shape[1], cf_imgs[0].shape[0])
 
             pre_trajectory_file = os.path.join(args.save_path, f'{cf_id}_pre_old.mp4')
