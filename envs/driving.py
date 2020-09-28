954849f2c5f95352693ed5aace2a9e4520b5d0b5
Julius <juliustfrost@gmail.com>
2020-09-28 19:43:28 -0400

    extend driving configuration options

--- envs/driving.py
+++ envs/driving.py
@@ -22,31 +22,43 @@ class DrivingSimulatorStateWrapper(SimulatorStateWrapper):
         return success
 
 
-def env_creator(**kwargs):
+def driving_creator(**kwargs):
     env = PLEEnv(
-        game_name=kwargs.get('game_name', 'Driving'),
-        display_screen=False,
-        num_steps=NUM_STEPS,
+        # cannot change
+        game_name='Driving',
+        # display screen on monitor
+        display_screen=kwargs.get('display_screen', False),
+        # whether to use image observations
+        img_input=kwargs.get('image_observations', False),
+        # whether to use continuous actions
+        continuous_actions=kwargs.get('continuous_actions', True),
+        # number of action repeat (frame skip)
+        num_steps=kwargs.get('action_repeat', NUM_STEPS),
+        # applicable only if using image observations
+        obs_width=kwargs.get('obs_width', 84),
+        # applicable only if using image observations
+        obs_height=kwargs.get('obs_height', 84),
+        # applicable only if using image observations
+        n_frames=kwargs.get('n_frames', 4),
+        # probability of cars switching lanes
         switch_prob=kwargs.get('switch_prob', 0.),
+        # time for cars to switch lanes
         switch_duration=kwargs.get('switch_duration', 50),
+        # dimension of actions
         action_dim=kwargs.get('action_dim', 2),
+        # reward penalty for collision
         collision_penalty=kwargs.get('collision_penalty', -100),
+        # proportion to use per time step reward bonus
         time_reward_proportion=kwargs.get('time_reward_proportion', 1),
+        # normalize rewards by dividing by constant
         normalize_reward=kwargs.get('normalize_reward', True),
+        # environment time step limit.
+        # agent time steps = environment time steps / action repeat
+        time_limit=kwargs.get('time_limit', 500)
     )
     env = DrivingSimulatorStateWrapper(env)
     return env
 
 
 def register():
-    def driving_acc_env_factory(env_config):
-        return env_creator(game_name="Driving",
-                           action_dim=env_config.get('action_dim', 2),
-                           switch_prob=env_config.get('switch_prob', 0.),
-                           switch_duration=env_config.get('switch_duration', 50),
-                           collision_penalty=env_config.get('collision_penalty', -100),
-                           time_reward_proportion=env_config.get('time_reward_proportion', 1),
-                           normalize_reward=env_config.get('normalize_reward', True),
-                           )
-
-    register_env('DrivingPLE-v0', driving_acc_env_factory)
+    register_env('DrivingPLE-v0', lambda env_config: driving_creator(**env_config))
