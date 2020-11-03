2cebdc2dfba33c636bd34c9f24957e3d6458e16e
Julius <juliustfrost@gmail.com>
2020-11-03 18:13:59 -0500

    fix driving_phase_1.sh to match the new configs

--- scc/driving_phase_1.sh
+++ scc/driving_phase_1.sh
@@ -15,13 +15,13 @@
 #$ -m bea
 
 # Give job a name
-#$ -N driving_p1
+#$ -N driving_p1_v2
 
 # Combine output and error files into a single file
 #$ -j y
 
 # Specify the output file name
-#$ -o driving_p1.qlog
+#$ -o driving_p1_v2.qlog
 
 # Request 8 CPUs
 #$ -pe omp 8
@@ -40,10 +40,7 @@ echo "Job ID : $JOB_ID  $SGE_TASK_ID"
 echo "=========================================================="
 
 # Use the SGE_TASK_ID environment variable to select the appropriate input file from bash array
-# Bash array index starts from 0, so we need to subtract one from SGE_TASK_ID value
-configs=(1.yaml 2.yaml 3.yaml 4.yaml 5.yaml 6.yaml 7.yaml 8.yaml 9.yaml)
-config_index=$(($SGE_TASK_ID-1))
-config=${configs[$config_index]}
+config_file=${SGE_TASK_ID}.yaml
 
 module load python3/3.7.7
 module load tensorflow/2.1.0
@@ -51,4 +48,4 @@ module load pytorch/1.6.0
 
 source /projectnb/saenkog/juliusf/cfrl-rllib/venv/bin/activate
 
-python train.py --ray-num-gpus 1 --ray-num-cpus 8 -f config/driving_phase_1/${config} 
+python train.py --ray-num-gpus 1 --ray-num-cpus 8 -f config/driving_phase_1/${config_file}
