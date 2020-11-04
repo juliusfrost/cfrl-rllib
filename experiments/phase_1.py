4212cfbe592229d805350586e00e44dafcffe7a4
Julius <juliustfrost@gmail.com>
2020-11-03 19:31:43 -0500

    don't merge the default configuration, which can cause errors. it is merged after in explain.py

--- experiments/phase_1.py
+++ experiments/phase_1.py
@@ -2,27 +2,23 @@ import argparse
 import json
 import os
 
-from ray.tune.utils import merge_dicts
+import yaml
 
-from explain import load_config, DEFAULT_CONFIG
-from explain import main as explain_main
 from experiments.utils.sampling import traverse_config
+from explain import main as explain_main
 
 
 def main(argv=None):
-    """
-
-    """
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--multi-config', type=str,
                         help='config with indicators for which to compare experiments')
-    parser.add_argument('--config', type=json.loads, default='{}', help='optional bypass config file')
+    parser.add_argument('--config', type=json.loads, default='{}', help='optional bypass multi config file')
 
     args = parser.parse_args(argv)
     if args.multi_config is not None and os.path.exists(args.multi_config):
-        multi_config = load_config(args.multi_config)
+        multi_config = yaml.safe_load(open(args.multi_config))
     else:
-        multi_config = merge_dicts(DEFAULT_CONFIG, args.config)
+        multi_config = args.config
 
     list_configs = traverse_config(multi_config)
 
