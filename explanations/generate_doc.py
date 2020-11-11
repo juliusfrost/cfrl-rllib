54498512eebea8d2cc269cd79b386369b4dd52cc
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-11-11 15:47:00 -0800

    update generate doc; not tested

--- explanations/generate_doc.py
+++ explanations/generate_doc.py
@@ -34,6 +34,9 @@ def get_explain_name(explanation_method, config):
         return NotImplementedError
     return get_name
 
+def get_eval_name(eval_dir, trial):
+    os.path.join(eval_dir, f'counterfactual_window-trial_{trial}.gif')
+
 
 def add_explanations(document, trial, image_path):
     p = document.add_paragraph('Explanation')
@@ -45,18 +48,16 @@ def add_explanations(document, trial, image_path):
         print(f'Could not find image {image_path}')
 
 
-def add_evaluations(document, trial, eval_images, eval_order):
+def add_evaluations(document, trial, eval_image):
     p = document.add_paragraph('Evaluation')
     p = document.add_paragraph('')
-    image_paths = list(eval_images[trial].values())
     r = p.add_run()
-    for i in eval_order[trial]:
-        r.add_picture(image_paths[i], height=Inches(2))
+    r.add_picture(eval_image, height=Inches(2))
     p = document.add_paragraph('')
     p = document.add_paragraph('Which continuation do you think came from the explained behavior policy? ')
 
 
-def build_document(save_dir, video_dir, explanation_method, num_trials, eval_images, eval_order, config):
+def build_document(save_dir, video_dir, explanation_method, num_trials, config):
     document = docx.Document()
     document.add_heading('Explainable Reinforcement Learning User Study', 0)
     p = document.add_paragraph()
@@ -65,10 +66,11 @@ def build_document(save_dir, video_dir, explanation_method, num_trials, eval_ima
     name_formula = get_explain_name(explanation_method, config)
     for trial in range(num_trials):
         explanation_dir = os.path.join(video_dir, f'explain-{explanation_method}')
+        eval_dir = os.path.join(video_dir, 'eval')
 
         document.add_heading(f'Trial {trial}', level=1)
         add_explanations(document, trial, name_formula(explanation_dir, trial))
-        add_evaluations(document, trial, eval_images, eval_order)
+        add_evaluations(document, trial, get_eval_name(eval_dir, trial))
 
     document.save(os.path.join(save_dir, f'{explanation_method}.docx'))
 
@@ -87,30 +89,6 @@ def read_eval_policies(video_dir):
     return eval_images
 
 
-def build_eval_order(num_trials: int, eval_images: dict):
-    """
-
-    :param num_trials: number of trials
-    :param eval_images: output of list_eval_images
-    :return:
-    """
-    eval_order = []
-    for i in range(num_trials):
-        num_policies = len(eval_images[i])
-        order = np.arange(num_policies)
-        np.random.shuffle(order)
-        eval_order.append(order.tolist())
-    return eval_order
-
-
-def save_eval_order(eval_order, save_dir):
-    output = {'trial': {}}
-    for i, trial in eval_order:
-        output['trial'][i] = trial
-    with open(os.path.join(save_dir, 'eval.json'), mode='w') as f:
-        json.dump(output, f)
-
-
 def main(parser_args=None):
     args = parse_args(parser_args)
 
@@ -118,11 +96,8 @@ def main(parser_args=None):
     explanation_methods = args.config['explanation_method']
     assert isinstance(explanation_methods, list)
 
-    eval_images = read_eval_policies(args.video_dir)
-    eval_order = build_eval_order(num_trials, eval_images)
     for explanation_method in explanation_methods:
-        build_document(args.save_dir, args.video_dir, explanation_method, num_trials, eval_images, eval_order,
-                       args.config)
+        build_document(args.save_dir, args.video_dir, explanation_method, num_trials, args.config)
 
 
 if __name__ == '__main__':
