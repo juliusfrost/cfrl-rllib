1e4e0b2da45d56b30cc336e43cb8097c81d95fe8
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-11-25 02:15:14 -0800

    don't comment out file removal

--- explain.py
+++ explain.py
@@ -316,8 +316,8 @@ def main(argv=None):
     elif stop == 'doc':
         generate_doc(config, video_dir, args.doc_id)
 
-    # for ext in config.get('remove_ext', []):
-    #     remove_ext(experiment_dir, ext)
+    for ext in config.get('remove_ext', []):
+        remove_ext(experiment_dir, ext)
 
 
 if __name__ == '__main__':
