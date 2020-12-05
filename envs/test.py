5ddf6aa58ca24eb13fac55f2d1acc5dbe674558c
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-12-04 17:44:01 -0800

    add policy to test script

--- envs/test.py
+++ envs/test.py
@@ -1,14 +1,42 @@
+import os
 import sys
 import time
-sys.path.append('.')
-
+import argparse
+import pickle as pkl
+import ray
+from ray.tune.registry import get_trainable_cls
+import torch
 import envs
 
+sys.path.append('.')
+ray.init()
 envs.register()
-import numpy as np  
-#env = env_creator()
+parser = argparse.ArgumentParser()
+parser.add_argument('--policy', default=None, help='policy to run')
+parser.add_argument('--algo', default='PPO', help='algorithm to run')
+args = parser.parse_args()
+
+# Load Policy
+if args.policy is not None:
+    config_dir = os.path.dirname(args.policy)
+    config_path = os.path.join(config_dir, "params.pkl")
+    # Try parent directory.
+    if not os.path.exists(config_path):
+        config_path = os.path.join(config_dir, "../params.pkl")
+    print("CONFIG PATH", config_path)
+    with open(config_path, "rb") as f:
+        config = pkl.load(f)
+    cls = get_trainable_cls(args.algo)
+    agent = cls(env=config["env"], config=config)
+    # Load state from checkpoint.
+    agent.restore(args.policy)
+    policy = agent.get_policy()
+else:
+    policy = None
+
+
 env = envs.driving.driving_creator(switch_prob=0.0)
-env.reset()
+obs = env.reset()
 env.render()
 done = False
 while not done:
@@ -28,7 +56,13 @@ while not done:
         saved_state, time_steps, sp, rng = env.game_state.game.getGameStateSave()
     if 'r' in a:
         env.game_state.game.setGameState(saved_state, time_steps, sp, rng)
-    _, _, done, _ = env.step(action)
+    if 'p' in a:
+        if policy is None:
+            print("No policy found!")
+        else:
+            logits, _ = policy.model.from_batch({"obs": torch.FloatTensor(obs).unsqueeze(0).to(policy.device)})
+            action_dist = policy.dist_class(logits, policy.model)
+            action = action_dist.sample().detach().cpu().numpy()[0]
+    print(action)
+    obs, _, done, _ = env.step(action)
     env.render()
-
-
