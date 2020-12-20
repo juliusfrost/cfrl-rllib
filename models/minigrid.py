e65a013f71f4faa9071da7ea774c9b37e9375562
Julius <juliustfrost@gmail.com>
2020-12-20 17:13:17 -0500

    fix imports

--- models/minigrid.py
+++ models/minigrid.py
@@ -1,14 +1,13 @@
 from typing import Dict, List
 
 import gym
-import torch
-import torch.nn as nn
-
 from ray.rllib.models import ModelCatalog
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
-from ray.rllib.utils.framework import TensorType
+from ray.rllib.utils.framework import TensorType, try_import_torch
 from ray.rllib.utils.typing import ModelConfigDict
 from ray.tune.utils import merge_dicts
+
+_, nn = try_import_torch()
 
 DEFAULT_CONFIG = {
     'conv_filters': [
