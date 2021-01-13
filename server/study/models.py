01b44ff8dddd67297fd0e13de2e93006f9f96494
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2021-01-13 03:38:37 -0500

    add new models

--- server/study/models.py
+++ server/study/models.py
@@ -1,32 +1,44 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Questionnaire(models.Model):
     name = models.CharField(max_length=1000)
+    num_trials = models.IntegerField()
+    intro_text = models.CharField(max_length=1000)
+    title_text = models.CharField(max_length=200)
+    explanation_study_text = models.CharField(max_length=1000)
+    eval_study_text = models.CharField(max_length=1000)
+    explain_heading_text = models.CharField(max_length=200)
+    eval_heading_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=1000)
 
 
-class User(models.Model):
+class Trial(models.Model):
     questionnaire = models.ForeignKey(Questionnaire, on_delete=models.CASCADE)
+    trial_heading_text = models.CharField(max_length=200)
+    order = models.IntegerField()
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=255)  # optional
-    questionnaire = models.ForeignKey(Questionnaire, models.CASCADE)
-    trial = models.IntegerField()
+class Explanation(models.Model):
+    trial = models.ForeignKey(Trial, on_delete=models.CASCADE)
+    static_path = models.CharField(max_length=1000)
 
 
-class Choice(models.Model):
-    choice_text = models.CharField(max_length=255)  # optional
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    votes = models.IntegerField(default=0)
+class Evaluation(models.Model):
+    trial = models.ForeignKey(Trial, on_delete=models.CASCADE)
+    static_path = models.CharField(max_length=1000)
+    num_choices = models.IntegerField()
+    solution = models.IntegerField()
+
+
+# User related classes
+class Respondents(models.Model):
+    questionnaire = models.ForeignKey(Questionnaire, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Response(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    trial = models.ForeignKey(Trial, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
-
-
-class Solution(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    choice = models.IntegerField()
