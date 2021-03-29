f6b6b98247741a8562a5ec03383316a55df97770
eweiner <eric@mcweiner.org>
2021-03-29 08:33:25 -0700

    Separate context and continuations

--- explain.py
+++ explain.py
@@ -231,7 +231,8 @@ def generate_evaluation_videos(config, dataset_file, video_dir):
     args += ['--policy-name', config['behavior_policy_config']['name']]
     args += ['--run', config['behavior_policy_config']['run']]
     args += ['--behavioral-policy', config['behavior_policy_config']['checkpoint']]
-    args += ['--side-by-side']
+    # args += ['--side-by-side']
+    args += ['--save-separate']
     if config['stop'] == 'html' and config['video_config']['format'] != 'mp4':
         print(f'When generating a html study, the video format must be mp4. '
               f'You are currently using {config["video_config"]["format"]}. '
