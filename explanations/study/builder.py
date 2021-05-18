594ee3276480a983435082ba0694f67742f5ad7b
Julius <juliustfrost@gmail.com>
2021-05-18 15:00:17 -0400

    add performance selection task to study builder

--- explanations/study/builder.py
+++ explanations/study/builder.py
@@ -4,7 +4,8 @@ import os
 import yaml
 from ray.tune.utils import merge_dicts
 
-from explanations.study.files import get_eval_names, get_explain_name, get_solutions, get_eval_name
+from explanations.study.files import get_eval_names, get_explain_name, get_solutions, get_eval_name, get_eval_rewards, \
+    get_explain_names, path_verifier
 from explanations.study.text import get_introduction_text, get_explain_study_text, get_eval_study_text, \
     get_title_text, get_explain_heading_text, get_eval_heading_text, get_question_text
 
@@ -32,6 +33,9 @@ class StudyBuilder:
         self.explanation_method = explanation_method
         self.num_trials = num_trials
         self.config = config
+        self.extension = self.config['video_config']['format']
+        self.task = self.config['eval_task']
+        self.policy_names = [p['name'] for p in self.config['eval_config']['eval_policies']]
         self.study_config = config.get('study_config')
         self.text_config = {}
         text_config_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'text_configs')
@@ -58,15 +62,16 @@ class StudyBuilder:
         intro_text = self.get_text('intro_text', get_introduction_text(self.text_config))
         title_text = self.get_text('title_text', get_title_text())
         self.build_intro(title_text, intro_text)
-        name_formula = get_explain_name(self.explanation_method, self.config,
-                                        extension=self.config['video_config']['format'])
         explanation_study_text = self.get_text('explanation_study_text',
                                                get_explain_study_text(self.explanation_method, self.text_config))
         eval_study_text = self.get_text('eval_study_text', get_eval_study_text(self.text_config, self.config))
         explain_heading_text = self.get_text('explain_heading_text', get_explain_heading_text())
         eval_heading_text = self.get_text('eval_heading_text', get_eval_heading_text())
         question_text = self.get_text('question_text', get_question_text(self.text_config))
-        solutions, num_choices = get_solutions(self.root_dir, self.config)
+        eval_rewards, num_choices = get_eval_rewards(self.root_dir)
+        if self.task == BEHAVIOR_CONTINUATION:
+            solutions, _ = get_solutions(self.root_dir, self.config)
+            self.build_config['solutions'] = solutions
 
         # save to build config
         self.build_config['name'] = self.name
@@ -78,13 +83,13 @@ class StudyBuilder:
         self.build_config['explain_heading_text'] = explain_heading_text
         self.build_config['eval_heading_text'] = eval_heading_text
         self.build_config['question_text'] = question_text
+        self.build_config['eval_rewards'] = eval_rewards
         self.build_config['num_choices'] = num_choices
-        self.build_config['solutions'] = solutions
         self.build_config['trial_heading_texts'] = []
         self.build_config['explain_video_paths'] = []
         self.build_config['eval_video_paths'] = []
         # TODO: make this change with config to allow multiple policy explanations
-        self.build_config['task_name'] = BEHAVIOR_CONTINUATION
+        self.build_config['task_name'] = self.config['eval_task']
         self.build_config = merge_dicts(self.build_config, self.study_config['build_config'])
 
         for trial in range(self.num_trials):
@@ -93,19 +98,30 @@ class StudyBuilder:
             trial_heading_text = f'Trial {trial + 1}'
             self.trial_heading(trial_heading_text)
             # TODO: make list of explanation paths for multiple policies
-            explain_video_path = name_formula(explanation_dir, trial)
+            if self.task == BEHAVIOR_CONTINUATION:
+                explain_video_paths = get_explain_name(self.explanation_method, self.config, explanation_dir, trial,
+                                                      self.extension)
+            elif self.task == PERFORMANCE_SELECTION:
+                explain_video_paths = get_explain_names(self.explanation_method, self.policy_names, self.config,
+                                                       explanation_dir, trial, self.extension)
+            else:
+                raise NotImplementedError
             if self.config['eval_config']['side_by_side']:
-                eval_video_paths = get_eval_name(eval_dir, trial, extension=self.config['video_config']['format'])
+                eval_video_paths = get_eval_name(eval_dir, trial, extension=self.extension)
             else:
                 eval_video_paths = get_eval_names(eval_dir, trial, self.build_config['num_choices'],
-                                                  extension=self.config['video_config']['format'])
-            self.add_explanations(explain_video_path, self.root_dir, explain_heading_text, explanation_study_text)
+                                                  extension=self.extension)
+            self.add_explanations(explain_video_paths, self.root_dir, explain_heading_text, explanation_study_text)
             self.add_evaluations(eval_video_paths, self.root_dir, eval_heading_text, eval_study_text, question_text)
 
             # save to build config
             self.build_config['trial_heading_texts'].append(trial_heading_text)
-            self.build_config['explain_video_paths'].append(explain_video_path)
+            self.build_config['explain_video_paths'].append(explain_video_paths)
             self.build_config['eval_video_paths'].append(eval_video_paths)
+
+        # verify paths exist
+        path_verifier(self.root_dir, self.build_config['explain_video_paths'])
+        path_verifier(self.root_dir, self.build_config['eval_video_paths'])
 
         self.build_outro()
         self.save_build_config()
